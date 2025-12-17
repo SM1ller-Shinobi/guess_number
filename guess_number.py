d133ca01a1494f704a45ef5e2275250e56d72568
Aleksandr Miller <ss.mmiller.2004@gmail.com>
2025-12-17 20:46:09 +0700

    First commit

---
+++ guess_number.py
@@ -0,0 +1,12 @@
+from random import randint
+number = randint(1,10)
+while True:
+    guess = int(input('Ваше число: '))
+    if guess < number: print(f'Ваше число меньше.')
+    elif guess > number: print(f'Ваше число больше.')
+    elif guess == number:
+        print(f'Вы победили!')
+        break
+    else:
+        print(f'неправльный формат.')
+        exit()
