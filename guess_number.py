5a969a69d1ecc6b88b615a6aaecf2df1ad3b651f
Aleksandr Miller <ss.mmiller.2004@gmail.com>
2025-12-17 20:54:46 +0700

    add enters

--- guess_number.py
+++ guess_number.py
@@ -2,8 +2,10 @@ from random import randint
 number = randint(1,10)
 while True:
     guess = int(input('Ваше число: '))
-    if guess < number: print(f'Ваше число меньше.')
-    elif guess > number: print(f'Ваше число больше.')
+    if guess < number:
+        print(f'Ваше число меньше.')
+    elif guess > number:
+        print(f'Ваше число больше.')
     elif guess == number:
         print(f'Вы победили!')
         break
